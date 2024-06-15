4cf6db2c8bee59b3fa7a6585f80cc3270c5190c2
Nevezhin Yan <yaniknezhin@gmail.com>
2024-06-15 06:36:38 +0500

    Bugs fix, config choising command add

---
+++ pkg/core/time/consts.go
@@ -0,0 +1,10 @@
+package time
+
+// Time and deadline constants are described below
+const (
+	FakeVotingMinSeconds = 5
+	FakeVotingMaxSeconds = 25
+)
+
+// Everything below is automatically calculated
+const ()
